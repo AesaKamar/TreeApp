63a196724116869a3e83670e8a920b2b7085915a
Aesa Kamar <Aesa.Kamar@Gmail.com>
2016-04-16 12:08:10 -0400

    Fixed attributes

--- api/models/Person.js
+++ api/models/Person.js
@@ -8,41 +8,40 @@
 module.exports = {
 
   attributes: {
-    attributes: {
-      first_name: {
-        type: 'string',
-        required: true
-      },
-      middle_name: {
-        type: 'string',
-        required: false
-      },
-      last_name: {
-        type: 'string',
-        required: true
-      },
-      date_of_birth: {
-        type: 'date',
-        required: true
-      },
-      pictures:{
-        collection: 'picture',
-        via: 'picture',
-        through: 'tag'
-      },
-      user: {
-        model: 'user',
-        required: false,
-      },
-      relations:{
-        collection: 'person',
-        via: 'related_to',
-        through: 'relation'
-      },
-      toJSON: function() {
-        var obj = this.toObject();
-        return obj;
-      }
+    first_name: {
+      type: 'string',
+      required: true
+    },
+    middle_name: {
+      type: 'string',
+      required: false
+    },
+    last_name: {
+      type: 'string',
+      required: true
+    },
+    date_of_birth: {
+      type: 'date',
+      required: false
+    },
+    pictures: {
+      collection: 'picture',
+      via: 'picture',
+      through: 'tag'
+    },
+    user: {
+      model: 'user',
+      required: false,
+    },
+    relations: {
+      collection: 'person',
+      via: 'related_to',
+      through: 'relation'
+    },
+    toJSON: function() {
+      var obj = this.toObject();
+      return obj;
     }
+
   }
 };
