70dc11cc51f3be641526735017d2d7509520c183
Sheik Kamar <szkamar@outlook.com>
2016-05-15 14:28:48 -0400

    fixed grunt clean for windows, recursive delete

--- tasks/config/clean.js
+++ tasks/config/clean.js
@@ -13,7 +13,7 @@
 module.exports = function(grunt) {
 
   grunt.config.set('clean', {
-    dev: ['.tmp/public/**'],
+    dev: ['.tmp/public/**/**/**/**/**','.tmp/public/**/**/**/**', '.tmp/public/**/**', '.tmp/public/**'],
     build: ['www']
   });
 
