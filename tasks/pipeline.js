98b056def6bcba58d5afb58066f2a8ca3c994cad
Aesa Kamar <Aesa.Kamar@gmail.com>
2016-05-18 11:24:24 -0400

    Fixed pipeline injector for angular-growl

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -17,7 +17,8 @@
 // (if you're using LESS with the built-in default config, you'll want
 //  to change `assets/styles/importer.less` instead.)
 var cssFilesToInject = [
-  'styles/**/*.css'
+  '/js/dependencies/angular-growl-v2/build/*.min.css',
+  'styles/**/*.css',
 ];
 
 
@@ -33,13 +34,13 @@ var jsFilesToInject = [
   // Dependencies like jQuery, or Angular are brought in here
   '/js/dependencies/module.js',
   '/js/dependencies/ng-file-upload/*.min.js',
-  '/js/dependencies/angular-growl-v2/build.*.min.*',
+  '/js/dependencies/angular-growl-v2/build/*.min.js',
   '/js/services/*.js',
 
 
   // All of the rest of your client-side js files
   // will be injected here in no particular order.
-  'js/controllers/**.js'
+  'js/controllers/**.js',
 ];
 
 
