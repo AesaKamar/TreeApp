41a08e1348153a14a79ba0db93db8adff7509f98
MatthewMawby <themesta@gmail.com>
2017-03-31 18:05:17 -0400

    navbar template file extension corrected

--- angular-frontend/_app/templates/navbar.template.js
+++
@@ -1,3 +0,0 @@
-<md-nav-bar md-selected-nav-item="vm.currentNavItem">
-    <md-nav-item ng-repeat="item in navItems" md-nav-click="goto('{{item.route}}')" name="{{item.label}}">{{item.label}}</md-nav-item>
-</md-nav-bar>
