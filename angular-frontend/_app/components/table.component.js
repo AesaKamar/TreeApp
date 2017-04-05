29f69bab8a0c96a126444c4cede3b2f557a7eac1
MatthewMawby <themesta@gmail.com>
2017-04-04 21:42:17 -0400

    added action items to list items in table component

--- angular-frontend/_app/components/table.component.js
+++ angular-frontend/_app/components/table.component.js
@@ -9,8 +9,8 @@
         },
         templateUrl: "_app/templates/table.template.html",
         controller: ['$scope', function($scope) {
-            $scope.People = [{'first_name': "Hank", 'last_name': "Snuffalufogus", 'image': "http://www.eyeglassboy.com/images/1bobdylan.jpg"}, {'first_name': "Bob",'last_name': "Dylan", 'image': "http://www.eyeglassboy.com/images/1bobdylan.jpg"}]
-            
+            $scope.People = [{'first_name': "Hank", 'last_name': "Snuffalufogus", 'image': "http://www.eyeglassboy.com/images/1bobdylan.jpg"}, {'first_name': "Bob",'last_name': "Dylan", 'image': "http://www.eyeglassboy.com/images/1bobdylan.jpg"}, {'first_name': "Darlene",'last_name': "Darlano", 'image': "http://www.eyeglassboy.com/images/1bobdylan.jpg"}, {'first_name': "Billy",'last_name': "TheAdult", 'image': "http://www.eyeglassboy.com/images/1bobdylan.jpg"}]
+
             console.log("In table Component");
         }]
     });
