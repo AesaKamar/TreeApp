36f66dfc966c86d9754407cbfc86e76538d24580
MatthewMawby <themesta@gmail.com>
2017-04-04 22:30:02 -0400

    styled table component

--- angular-frontend/_app/components/table.component.js
+++ angular-frontend/_app/components/table.component.js
@@ -8,6 +8,7 @@
             screen: "<"
         },
         templateUrl: "_app/templates/table.template.html",
+        styles: ['div { background-color: #fdfdfd; }'],
         controller: ['$scope', function($scope) {
             $scope.People = [{'first_name': "Hank", 'last_name': "Snuffalufogus", 'image': "http://www.eyeglassboy.com/images/1bobdylan.jpg"}, {'first_name': "Bob",'last_name': "Dylan", 'image': "http://www.eyeglassboy.com/images/1bobdylan.jpg"}, {'first_name': "Darlene",'last_name': "Darlano", 'image': "http://www.eyeglassboy.com/images/1bobdylan.jpg"}, {'first_name': "Billy",'last_name': "TheAdult", 'image': "http://www.eyeglassboy.com/images/1bobdylan.jpg"}]
 
