a28f6c98cf38d3319a8f01edcc6c56fdf82b5995
MatthewMawby <themesta@gmail.com>
2017-04-04 23:22:54 -0400

    Person model filtered by first name & last name

--- angular-frontend/_app/components/table.component.js
+++ angular-frontend/_app/components/table.component.js
@@ -10,7 +10,15 @@
         templateUrl: "_app/templates/table.template.html",
         styles: ['div { background-color: #fdfdfd; }'],
         controller: ['$scope', function($scope) {
-            $scope.People = [{'first_name': "Hank", 'last_name': "Snuffalufogus", 'image': "http://www.eyeglassboy.com/images/1bobdylan.jpg"}, {'first_name': "Bob",'last_name': "Dylan", 'image': "http://www.eyeglassboy.com/images/1bobdylan.jpg"}, {'first_name': "Darlene",'last_name': "Darlano", 'image': "http://www.eyeglassboy.com/images/1bobdylan.jpg"}, {'first_name': "Billy",'last_name': "TheAdult", 'image': "http://www.eyeglassboy.com/images/1bobdylan.jpg"}]
+            $scope.People = [{'first_name': "Hank", 'last_name': "Snuffalufogus", 'image': "http://www.eyeglassboy.com/images/1bobdylan.jpg"}, {'first_name': "Bob",'last_name': "Dylan", 'image': "http://www.eyeglassboy.com/images/1bobdylan.jpg"}, {'first_name': "Darlene",'last_name': "Darlano", 'image': "http://www.eyeglassboy.com/images/1bobdylan.jpg"}, {'first_name': "Billy",'last_name': "TheAdult", 'image': "http://www.eyeglassboy.com/images/1bobdylan.jpg"},{'first_name': "Billy",'last_name': "TheKid", 'image': "http://www.eyeglassboy.com/images/1bobdylan.jpg"}]
+
+            $scope.query = '';
+
+            $scope.personFilter = function (person) {
+                var query = $scope.query.toLowerCase(),
+                name = person.first_name.toLowerCase() + ' ' + person.last_name.toLowerCase();
+                return name.indexOf(query) > -1;
+            };
 
             console.log("In table Component");
         }]
