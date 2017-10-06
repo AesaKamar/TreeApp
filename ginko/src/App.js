658891879cd1ab92f074cdee9ba8d83eaf41490d
adeet <blaze5545@gmail.com>
2017-10-05 20:45:40 -0400

    adding unit testing framework (jest / enzyme)

--- ginko/src/App.js
+++ ginko/src/App.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react'
 import './App.css'
+import Main from './components/Main.js'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title"> {'Ginko Family Tree'}</h1>
-        </header>
-        <p className="App-intro">
-          {'This is our app!'}
-        </p>
-      </div>
+      <Main/>
     )
   }
 }
