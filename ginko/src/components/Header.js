c503d61183866325d4114d4c7492041ac07ca792
TheoRice <theo.rice1996@gmail.com>
2017-10-21 19:55:44 -0400

    began navbar css and added matt's colors.css file to project

--- ginko/src/components/Header.js
+++ ginko/src/components/Header.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
+import '../styles/header-style.css'
+
 class Header extends Component {
   render() {
     return (
       <nav>
-        <ul>
-          <li><Link to="/login">{'Login'}</Link></li>
-          <li><Link to="/graph">{'Graph'}</Link></li>
-        </ul>
+        <div id="navbar">
+          <ul className="flex-container">
+            <li className="flex-item"><Link to="/login">{'Login'}</Link></li>
+            <li className="flex-item"><Link to="/graph">{'Graph'}</Link></li>
+          </ul>
+        </div>
       </nav>
     )
   }
