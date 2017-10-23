1afbd91a7f511a4b1d940dfe15dce16b81ec22e9
Matthew Mawby <matthew@Matthews-MBP.nycap.rr.com>
2017-10-23 01:58:14 -0400

    Header dynamically generates navItems & header styling updated

--- ginko/src/components/Header.js
+++ ginko/src/components/Header.js
@@ -1,19 +1,73 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
-import '../styles/header-style.css'
+import '../styles/header.css'
 
 class Header extends Component {
+  constructor() {
+    super()
+    // to add a new navItem, add a new object to routeArray
+    let routeArray = [
+      {
+        path: '/login',
+        text: 'Login',
+        id: 0
+      },
+      {
+        path: '/graph',
+        text: 'Graph',
+        id: 1
+      }
+    ]
+    this.state = {
+      routes: routeArray,
+      navItemSelected: 0
+    }
+  }
+
+  state: {
+    routes: [any],
+    navItemSelected: number
+  };
+
+  setActive(id:number) {
+    this.setState({ navItemSelected: id })
+  }
+
+  // render a link in the navbar
+  renderNavItem(navItem: {path:string, text:string, id:number}) {
+    // determine css class
+    let linkAccentClass = null
+    if (this.state.navItemSelected === navItem.id) {
+      linkAccentClass = 'link-accent-selected'
+    } else {
+      linkAccentClass = 'link-accent'
+    }
+    // render navItem
+    return (
+      <div className="nav-item" key={navItem.id}>
+        <div className="link-box">
+          <Link onClick={this.setActive.bind(this, navItem.id)}
+            to={navItem.path}
+          >
+            {navItem.text}
+          </Link>
+        </div>
+        <div className={linkAccentClass} />
+      </div>
+    )
+  }
+
   render() {
     return (
-      <nav>
-        <div id="navbar">
-          <ul className="flex-container">
-            <li className="flex-item"><Link to="/login">{'Login'}</Link></li>
-            <li className="flex-item"><Link to="/graph">{'Graph'}</Link></li>
-          </ul>
+      <div className="nav-bar">
+        <div className="link-container">
+          {this.state.routes.map(routeInfo => (
+            this.renderNavItem(routeInfo)
+          ))}
         </div>
-      </nav>
+        <div className="nav-accent"/>
+      </div>
     )
   }
 }
--- ginko/src/index.js
+++ ginko/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './App'
+import './styles/index.css'
+import App from './components/App'
 
 ReactDOM.render((
   <BrowserRouter>
