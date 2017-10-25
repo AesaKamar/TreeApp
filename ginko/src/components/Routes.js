165019f461d23f0bb91877dbc13dc6dfd9c61266
Theodore Rice <theo.rice1996@gmail.com>
2017-10-24 23:18:05 -0400

    Instructions, highlighting bug, and new views/routes for People and Profile (#91)
    * fixed hightlighting issue on nav-item, and added additional routes and views for People and Profile
    
    * Updated readme to include instructions for running angular app

--- ginko/src/components/Routes.js
+++ ginko/src/components/Routes.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import { Switch, Route } from 'react-router-dom'
 import GraphView from '../views/GraphView'
 import LoginView from '../views/LoginView'
+import PeopleView from '../views/PeopleView'
+import ProfileView from '../views/ProfileView'
 
 class Routes extends Component {
   render() {
@@ -10,6 +12,8 @@ class Routes extends Component {
         <Route exact path="/" component={LoginView}/>
         <Route exact path="/graph" component={GraphView}/>
         <Route exact path="/login" component={LoginView}/>
+        <Route exact path="/people" component={PeopleView}/>
+        <Route exact path="/profile" component={ProfileView}/>
       </Switch>
     )
   }
